13e5067f3f05a963f0595cf7e3b920e6d7109be6
negin <83733021+negindaryadel@users.noreply.github.com>
2021-11-17 15:17:27 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i , j , n ,m ;
+    cout<<"Enter n: "<<endl;
+    cin>>n;
+    cout<<"Enter m: "<<endl;
+    cin>>m;
+    cout<<endl;
+    for(i=1;i<=n;i++)
+    {
+
+        for(j=1; j<=m ; j++)
+            cout<<i*j<<'\t';
+        cout<<'\n';
+    }
+    cout<<'\n\n\n'<<"exit";
+    return 0;
+}
+
+
